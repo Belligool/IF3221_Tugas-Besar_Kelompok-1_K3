2f1ff972f84ce04c2062f63af8e876bc408f3438
Bagas Noor Fadhilah <baagass2018@gmail.com>
2026-05-22 14:51:36 +0700

    feat: added feature selection using random forest and chi-square for unitigs selection

--- utilities/utility.py
+++ utilities/utility.py
@@ -1,31 +1,52 @@
 # Preprocessor class that will be the main pipeline for our project
+# This pipeline is limited to processing just 3 drugs
 
 import pandas as pd
 import os
 
 from pathlib import Path
 from typing import Literal
+from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import chi2_contingency
 
 class AMRDataPipeline:
     def __init__(self, rtab_path: str, 
                  metadata_path: str, 
-                 antibiotic_col: Literal['cfx_sr','azm_sr','cip_sr'],
-                 mode: Literal['auto','custom'],
-                 cols_to_dropped: list[str] = None,
+                 antibiotic_col: Literal['cfx','azm','cip'],
+                 mode: Literal['auto','preserve'],
+                 selection_mode: Literal['rf','chi'] = None,
+                 alpha_threshold: float = 0.05,
+                 n_features: int = 200,
                  index_name: str = "Sample_ID",
                  ):
         self.rtab_path = Path(rtab_path)
         self.metadata_path = Path(metadata_path)
         self.antibiotic_col = antibiotic_col
         self.index_name = index_name
+        self.mode = mode
+        self.selection_mode = selection_mode
+
         self.unitig_df = None
         self.metadata_df = None
         self.final_df = None
-        self.mode = mode
-        self.preserved_metadata_cols = [antibiotic_col, index_name]
+        self.preserved_metadata_cols = [f'{antibiotic_col}_sr', index_name]
 
-        if cols_to_dropped is not None:
-            self.cols_to_dropped = [cols for cols in cols_to_dropped if cols not in self.preserved_metadata_cols]
+        self._validate_pipeline(n_features=n_features, alpha_threshold=alpha_threshold)
+
+    def _validate_pipeline(self, n_features: int = None, alpha_threshold: float = None) -> None:
+        if self.selection_mode is not None:
+            if self.selection_mode == 'rf':
+                if n_features is None:
+                    raise ValueError("When tree based feature selection is selected the number of features must be above 0.")
+                else:
+                    self.n_features = n_features
+            elif self.selection_mode == 'chi':
+                if alpha_threshold is None:
+                    raise ValueError("When statistical feature selection is applied the significance must be above 0.")
+                else:
+                    self.alpha_threshold = alpha_threshold
+        else:
+            return
 
     def _load_transpose_rtab(self) -> pd.DataFrame | None:
         # Read the path and set 'pattern_id' as their structural anchor with automatic seperator detector
@@ -42,23 +63,89 @@ class AMRDataPipeline:
     def _load_clean_metadata(self) -> pd.DataFrame | None:
         # Obtain metadata
         self.metadata_df = pd.read_csv(self.metadata_path)
+
         
         # Auto dropping cols and cleaning based on pipeline initialization
         if self.mode == 'auto':
             self.cols_to_dropped = [cols for cols in self.metadata_df.columns.tolist() if cols not in self.preserved_metadata_cols]
             self.metadata_df = self.metadata_df.drop(columns=self.cols_to_dropped)
-            self.metadata_df = self.metadata_df.dropna(subset=[self.antibiotic_col], axis=0)
 
-            # Preserves true and throws false based on boolean masking
-            self.metadata_df = self.metadata_df[self.metadata_df[self.antibiotic_col].isin([0,1])]
-            self.metadata_df[self.antibiotic_col] = self.metadata_df[self.antibiotic_col].astype('int8')
-            self.metadata_df.set_index(self.index_name, inplace=True)
-        elif self.mode == 'custom':
-            # Belom jadi
-            self.metadata_df.drop(columns=self.cols_to_dropped, inplace=True)
+        elif self.mode == 'preserve':
+
+            # Clean MIC cols
+            mic_cols = [c for c in self.metadata_df.columns if c.endswith("_mic") and not c.startswith("log2_")]
+            for col in mic_cols:
+                self.metadata_df[col] = self.metadata_df[col].astype(str).str.replace(r'[<>=]', '', regex=True)
+                self.metadata_df[col] = pd.to_numeric(self.metadata_df[col], errors='coerce')
+
+            # Clean Beta.lactamase
+            self.metadata_df['Beta.lactamase'] = self.metadata_df['Beta.lactamase'].astype(str).str.replace(r'r[<>=]','', regex=True)
+            self.metadata_df['Beta.lactamase'] = pd.to_numeric(self.metadata_df['Beta.lactamase'], errors='coerce')
+
+            # Filter columns
+            preserved_cols = []
+            variants = [f'{self.antibiotic_col}_mic',f'log2_{self.antibiotic_col}_mic',f'{self.antibiotic_col}_sr']
+            preserved_cols.extend([c for c in variants])
+            preserved_cols.extend(['Year','Country','Continent','Group',self.index_name,'Beta.lactamase'])
+
+            self.metadata_df = self.metadata_df[preserved_cols]
+            
+        # Drop any row that doesnt have a value for the target variable [1,0]
+        self.metadata_df = self.metadata_df.dropna(subset=[f'{self.antibiotic_col}_sr'], axis=0)
+
+        # Preserves true and throws false based on boolean masking
+        self.metadata_df = self.metadata_df[self.metadata_df[f'{self.antibiotic_col}_sr'].isin([0,1])]
+        self.metadata_df[f'{self.antibiotic_col}_sr'] = self.metadata_df[f'{self.antibiotic_col}_sr'].astype('int8')
+        self.metadata_df.set_index(self.index_name, inplace=True)
 
         return self.metadata_df
+    
+    def _rf_feature_selection(self) -> list[str]:
+        X_temp = self.unitig_df.join(self.metadata_df, how="inner")  
+        y_temp = X_temp[f"{self.antibiotic_col}_sr"]
+        X_temp = X_temp.drop(columns=[f"{self.antibiotic_col}_sr"] + self.metadata_df.columns.tolist(), errors='ignore')
+
+        print(X_temp.columns)
+
+        selector = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+        selector.fit(X_temp, y_temp)
+
+        importances = pd.Series(selector.feature_importances_, index=X_temp.columns)
+        top_unitigs = importances.nlargest(self.n_features).index.to_list()
+
+        return top_unitigs
+    
+    def _chi_feature_selection(self) -> list[str]:
+        X_temp = self.unitig_df.join(self.metadata_df, how="inner")
+        y_temp = X_temp[f"{self.antibiotic_col}_sr"]
+        X_temp = X_temp.drop(columns=[f"{self.antibiotic_col}_sr"] + self.metadata_df.columns.to_list(), errors='ignore')
+
+        num_test = X_temp.shape[1]
+
+        # Bonforreni Correction to make significane more appropriate and doesnt accumulate
+        adjusted_alpha = self.alpha_threshold / num_test
+
+        passed_unitigs = []
+
+        for unitig in X_temp.columns:
+            vector = X_temp[unitig].values
+            contigency_table = pd.crosstab(vector, y_temp)
+
+            # Unitig is present across all samples
+            if contigency_table.shape != (2,2):
+                continue
+            
+            # Yates correction
+            chi_2_stat, p_val, dof, expected = chi2_contingency(contigency_table, correction=True)
+
+            if p_val < adjusted_alpha:
+                passed_unitigs.append(unitig)
+
+        if len(passed_unitigs) == 0:
+            raise ValueError("Zero unitigs passed during statistical threshold using chi square.")
         
+        return passed_unitigs
+
     def _merge(self) -> pd.DataFrame | None:
         self.final_df = self.unitig_df.join(self.metadata_df, how="inner")        
 
@@ -71,8 +158,8 @@ class AMRDataPipeline:
         base_dir = Path( os.getcwd() + "/data")
         base_dir.mkdir(parents=True, exist_ok=True)
 
-        y = self.final_df[self.antibiotic_col]
-        X = self.final_df.drop(columns=[self.antibiotic_col])
+        y = self.final_df[f'{self.antibiotic_col}_sr']
+        X = self.final_df.drop(columns=[f'{self.antibiotic_col}_sr'])
 
         y_out = base_dir / f"{self.antibiotic_col}_labels.csv"
         X_out = base_dir / f"{self.antibiotic_col}_features.csv"
@@ -85,11 +172,27 @@ class AMRDataPipeline:
     def preprocess(self) -> pd.DataFrame:
         self._load_transpose_rtab()
         self._load_clean_metadata()
-        return self._merge()
+
+        if self.selection_mode is not None:
+            if self.selection_mode == 'rf':
+                top_unitigs = self._rf_feature_selection()
+            elif self.selection_mode == 'chi':
+                top_unitigs = self._chi_feature_selection()
+
+        columns_to_keep = top_unitigs + self.metadata_df.columns.to_list()
+        self._merge()
+        self.final_df = self.final_df[[c for c in columns_to_keep if c in self.final_df.columns]]
+        
+        return self.final_df
 
 
 if __name__ == "__main__":
-    pipeline = AMRDataPipeline("./data/azm_sr_gwas_filtered_unitigs.Rtab", "./data/metadata.csv", antibiotic_col='azm_sr', mode='auto')
+    pipeline = AMRDataPipeline("./data/azm_sr_gwas_filtered_unitigs.Rtab", 
+                               "./data/metadata.csv", 
+                               antibiotic_col='azm', 
+                               mode='preserve', 
+                               selection_mode='chi',
+                               n_features=200)
 
     test = pipeline.preprocess()
     print(test.shape)
